7061da9fe583f57800b915a9866399f98fea57bb
Isaiah Solomon <isaiah.c.solomon@gmail.com>
2021-06-29 21:04:41 -0700

    added google sign in

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,5 +32,23 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     `gatsby-plugin-offline`,
+    {
+      resolve: "gatsby-plugin-firebase",
+      options: {
+        credentials: {
+          apiKey: process.env.API_KEY,
+          authDomain: process.env.AUTH_DOMAIN,
+          databaseURL: process.env.DATABASE_URL,
+          projectId: process.env.PROJECT_ID,
+          storageBucket: process.env.STORAGE_BUCKET,
+          messagingSenderId: process.env.MESSAGE_SENDER_ID,
+          appId: process.env.APP_ID
+        }
+      }
+    },
+    {
+      resolve: `gatsby-plugin-create-client-paths`,
+      options: { prefixes: [`/admin/*`] },
+    },
   ],
 }
