58892310a16561eb0d430f46a6e87e87054abd8b
Isaiah Solomon <isaiah.c.solomon@gmail.com>
2021-07-05 15:57:25 -0700

    add base GA

--- gatsby-config.js
+++ gatsby-config.js
@@ -73,5 +73,21 @@ module.exports = {
         ],
       },
     },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-7M9C0QJXSM", // Google Analytics / GA
+        ],
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: false,
+          // Avoids sending pageview hits from custom paths
+          exclude: [],
+        },
+      },
+    },
   ],
 }
